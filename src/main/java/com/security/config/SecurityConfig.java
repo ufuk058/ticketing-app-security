a7329ff9144f2827a0b24c98b1f5d746a3a3d5b7
ufuk058 <ufuksahinduran.uk@gmail.com>
2024-11-26 11:36:49 +0000

    Spring basic authentication page matched required application files and pop up created by requstMatchers

--- src/main/java/com/security/config/SecurityConfig.java
+++ src/main/java/com/security/config/SecurityConfig.java
@@ -2,12 +2,15 @@ package com.security.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -27,5 +30,18 @@ public class SecurityConfig  {
         userList.add(user1); userList.add(user2);
 
         return new InMemoryUserDetailsManager(userList);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception{
+
+        return httpSecurity
+                .authorizeHttpRequests(authorize-> authorize
+                        .requestMatchers("/","/login","/fragments/**","/assets/**","/images/**").permitAll()
+                        .anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults())
+                .build();
+
+
     }
 }
